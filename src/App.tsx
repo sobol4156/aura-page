131f77609dc01c96dd6ce560a3799b53f51132aa
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-02 02:26:19 +0300

    header

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './components/Header_1/Header'
+function App() {
+
+
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header_1/Header.tsx
@@ -0,0 +1,25 @@
+import './Header.less'
+import Logo from './svg/logo.svg';
+// import Wallet from './svg/wallet.svg'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header__content'>
+        <a className='logo' href="#"><img src={Logo} alt="" /></a>
+        <nav>
+          <ul>
+            <li>Авторы</li>
+            <li>Коллекции</li>
+            <li>Сервисы</li>
+          </ul>
+        </nav>
+        <button className='btn-wallet'>Кошелёк</button>
+        {/* <a className='icon-wallet' href="#"><img src={Wallet} alt="" /></a> */}
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
