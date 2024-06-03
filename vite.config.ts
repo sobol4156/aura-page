fdec403f9cfb658daff541c14b082aabae7a9f70
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-03 17:39:36 +0300

    linter is ready

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/aura-page/",
   plugins: [svgr(),react()],
   css: {
     preprocessorOptions: {
