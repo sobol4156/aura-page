2719ee780f1d9e21ef1f429a203423d9161fae64
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-03 14:42:55 +0300

    auction title

--- src/components/Auction_3/Aiction_3.tsx
+++ src/components/Auction_3/Aiction_3.tsx
@@ -4,45 +4,48 @@ import Witcher from './svg/Witcher.svg'
 const Aiction_3 = () => {
     return (
         <section className='auction'>
-            <h2>Аукцион <span></span></h2>
+
             <div className='auction__content'>
-                <div>
-                    <img src={Witcher} alt="" />
-                    <span>Live auction</span>
-                </div>
-                <div>
-                    <div>
-                        <img src="" alt="" />
-                        <span>Serge_VM222</span>
+                <h2 className='auction__title'>Аукцион <span>Live auction</span></h2>
+                <div className='auction__bet-content'>
+                    <div className='auction__logo'>
+                        <img src={Witcher} alt="" />
                     </div>
-                    <h3>Witcherrr_3 (xml_t5)</h3>
-                    <div>
-                        <div>
-                            <span>Ставка:</span>
-                            <span>0.10 ETH</span>
-                            <span>$351.24</span>
+                    <div className='auction__bet'>
+                        <div className='auction__bet-user'>
+                            <img className='auction__bet-avatar' src="" alt="" />
+                            <span className='auction__bet-nickName'>Serge_VM222</span>
                         </div>
+                        <h3 className='auction__bet-subtitle'>Witcherrr_3 (xml_t5)</h3>
                         <div>
-                            <span>Окончание через:</span>
-                            <div>
-                                <div>
-                                    <span>01</span>
-                                    <span>Час</span>
-                                </div>
-                                <div>
-                                    <span>48</span>
-                                    <span>Мин</span>
-                                </div>
-                                <div>
-                                    <span>47</span>
-                                    <span>Сек</span>
+                            <div className='auction__bet-info'>
+                                <span className='auction__bet-caption'>Ставка:</span>
+                                <span className='auction__bet-price'>0.10 ETH</span>
+                                <span className='auction__bet-dollar'>$351.24</span>
+                            </div>
+                            <div className='auction__timer'>
+                                <span className='auction__timer-caption'>Окончание через:</span>
+                                <div className='auction__timer-content'>
+                                    <div className='auction__timer-hours'>
+                                        <span className='auction__timer-value'>01</span>
+                                        <span className='auction__timer-text'>Час</span>
+                                    </div>
+                                    <div className='auction__timer-minutes'>
+                                        <span className='auction__timer-value'>48</span>
+                                        <span className='auction__timer-text'>Мин</span>
+                                    </div>
+                                    <div className='auction__timer-seconds'>
+                                        <span className='auction__timer-value'>47</span>
+                                        <span className='auction__timer-text'>Сек</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <button className='auction__bet-btn'>
+                            <span>Сделать ставку</span>
+                        </button>
                     </div>
-                    <button>
-                        <span>Сделать ставку</span>
-                    </button>
+
                 </div>
             </div>
         </section>
