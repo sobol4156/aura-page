36d093284e3ad5f146805c9bfec762d0274135fe
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-03 21:59:55 +0300

    added adaptive in section-3

--- src/components/OtherAuction_4/OtherAuction_4.tsx
+++ src/components/OtherAuction_4/OtherAuction_4.tsx
@@ -6,12 +6,127 @@ const OtherAuction_4 = () => {
             <div className='otherAction__container'>
                 <h2 className='otherAction__title'>Другие аукционы</h2>
                 <ul className='otherAction__list'>
-                    <li className='otherAction__item'></li>
-                    <li className='otherAction__item'></li>
-                    <li className='otherAction__item'></li>
-                    <li className='otherAction__item'></li>
-                    <li className='otherAction__item'></li>
-                    <li className='otherAction__item'></li>
+                    <li className='otherAction__item'>
+                        <article className='otherAction__product'>
+                            <div className='otherAction__img'><img src="https://live.verstaem.online/aura/img/auction-1.png" alt="" /></div>
+                            <div className='otherAction__info'>
+                                <div className='otherAction__owner'>
+                                    <img src="https://live.verstaem.online/aura/img/avatar-1.jpg" alt="" />
+                                    <span>Frank.Nagval</span>
+                                </div>
+                                <h3 className='otherAction__item-title'>Alien (green) e_e</h3>
+                                <span className='otherAction__item-price'>0.02 ETH</span>
+                                <span className='otherAction__item-time'>03 часа 25 мин 23 сек</span>
+                            </div>
+                        </article>
+
+                    </li>
+                    <li className='otherAction__item'>
+                        <article className='otherAction__product'>
+                            <div className='otherAction__img'>
+                                <img src="https://live.verstaem.online/aura/img/auction-2.png" alt="" />
+                            </div>
+                            <div className='otherAction__info'>
+                                <div className='otherAction__owner'>
+                                    <img src="https://live.verstaem.online/aura/img/avatar-2.jpg" alt="" />
+                                    <span>CPH_777</span>
+                                </div>
+                                <h3 className='otherAction__item-title'>CoBRRRa</h3>
+                                <span className='otherAction__item-price'>0.5 ETH</span>
+                                <span className='otherAction__item-time'>15 мин 03 сек</span>
+                            </div>
+                        </article>
+
+                    </li>
+                    <li className='otherAction__item'>
+                        <article className='otherAction__product'>
+                            <div className='otherAction__img'><img src="https://live.verstaem.online/aura/img/auction-3.png" alt="" /></div>
+                            <div className='otherAction__info'>
+                                <div className='otherAction__owner'>
+                                    <img src="https://live.verstaem.online/aura/img/avatar-3.jpg" alt="" />
+                                    <span>sUpah_kiLLah</span>
+                                </div>
+                                <h3 className='otherAction__item-title'>Diamonds (4)</h3>
+                                <span className='otherAction__item-price'>0.008 ETH</span>
+                                <span className='otherAction__item-time'>23 часа 05 мин 08 сек</span>
+                            </div>
+                        </article>
+
+                    </li>
+                    <li className='otherAction__item'>
+                        <article className='otherAction__product'>
+                            <div className='otherAction__img'><img src="https://live.verstaem.online/aura/img/auction-4.png" alt="" /></div>
+                            <div className='otherAction__info'>
+                                <div className='otherAction__owner'>
+                                    <img src="https://live.verstaem.online/aura/img/avatar-4.jpg" alt="" />
+                                    <span>ttgs</span>
+                                </div>
+                                <h3 className='otherAction__item-title'>Holofobia-2</h3>
+                                <span className='otherAction__item-price'>1.0 ETH</span>
+                                <span className='otherAction__item-time'>01 час 15 мин 55 сек</span>
+                            </div>
+                        </article>
+
+                    </li>
+                    <li className='otherAction__item'>
+                        <article className='otherAction__product'>
+                            <div className='otherAction__img'><img src="https://live.verstaem.online/aura/img/auction-5.png" alt="" /></div>
+                            <div className='otherAction__info'>
+                                <div className='otherAction__owner'>
+                                    <img src="https://live.verstaem.online/aura/img/avatar-5.jpg" alt="" />
+                                    <span>Peppppe22</span>
+                                </div>
+                                <h3 className='otherAction__item-title'>Baby Yoda 1</h3>
+                                <span className='otherAction__item-price'>0.2 ETH</span>
+                                <span className='otherAction__item-time'>01 час 28 мин 11 сек</span>
+                            </div>
+                        </article>
+
+                    </li>
+                    <li className='otherAction__item'>
+                        <article className='otherAction__product'>
+                            <div className='otherAction__img'><img src="https://live.verstaem.online/aura/img/auction-6.png" alt="" /></div>
+                            <div className='otherAction__info'>
+                                <div className='otherAction__owner'>
+                                    <img src="https://live.verstaem.online/aura/img/avatar-6.jpg" alt="" />
+                                    <span>1300e1440</span>
+                                </div>
+                                <h3 className='otherAction__item-title'>alien vs. predator (01)</h3>
+                                <span className='otherAction__item-price'>0.09 ETH</span>
+                                <span className='otherAction__item-time'>08 часов 44 мин 43 сек</span>
+                            </div>
+                        </article>
+
+                    </li>
+                    <li className='otherAction__item'>
+                        <article className='otherAction__product'>
+                            <div className='otherAction__img'><img src="https://live.verstaem.online/aura/img/auction-7.png" alt="" /></div>
+                            <div className='otherAction__info'>
+                                <div className='otherAction__owner'>
+                                    <img src="https://live.verstaem.online/aura/img/avatar-7.jpg" alt="" />
+                                    <span>Robert_Wward</span>
+                                </div>
+                                <h3 className='otherAction__item-title'>IAMDECEPTICON</h3>
+                                <span className='otherAction__item-price'>0.1 ETH</span>
+                                <span className='otherAction__item-time'>55 мин 18 сек</span>
+                            </div>
+                        </article>
+                    </li>
+                    <li className='otherAction__item'>
+                        <article className='otherAction__product'>
+                            <div className='otherAction__img'><img src="https://live.verstaem.online/aura/img/auction-8.png" alt="" /></div>
+                            <div className='otherAction__info'>
+                                <div className='otherAction__owner'>
+                                    <img src="https://live.verstaem.online/aura/img/avatar-8.jpg" alt="" />
+                                    <span>ttgs</span>
+                                </div>
+                                <h3 className='otherAction__item-title'>Ghostbusterrrrrrrs</h3>
+                                <span className='otherAction__item-price'>0.09 ETH</span>
+                                <span className='otherAction__item-time'>18 мин 12 сек</span>
+                            </div>
+                        </article>
+
+                    </li>
                 </ul>
                 <div className='otherAction__btn-center'>
                     <button className='otherAction__btn'>Все аукционы</button>
