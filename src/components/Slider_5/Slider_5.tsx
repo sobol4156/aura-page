5dc49677c86e00763ccbc8409dc078d76b6700e5
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-04 21:59:01 +0300

    added animation slider not adaptive

--- src/components/Slider_5/Slider_5.tsx
+++ src/components/Slider_5/Slider_5.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react'
 import './Slider_5.less'
 
 const Slider_5 = () => {
+    const [currentSlide, setCurrenSlide] = useState(0)
+
+    const animationScene = () => {
+
+        switch (currentSlide) {
+            case 0:
+                return 'animate1'
+            case 1:
+                return 'animate2'
+            case 2:
+                return 'animate3'
+            case 3:
+                return 'animate4'
+            default:
+                return ''
+        }
+        
+    }
+
     return (
         <section className="slider">
             <div className="slider__content">
                 <h2 className='slider__title'><img src="https://live.verstaem.online/aura/img/fire.png" alt="" /> <span>Новые стикеры</span></h2>
                 <div className='slider__wrapper'>
-                    <div className='slider__current'>
+                    <div className={`slider__current ${animationScene()}`}>
                         <div className='slide1 slider-slide'>
                             <picture>
                                 <img src="https://live.verstaem.online/aura/img/new-1.png" alt="" />
@@ -46,10 +66,26 @@ const Slider_5 = () => {
                 </div>
                 <div className='slider__buttons'>
                     <div className='slider__menu-btn'>
-                        <div className='slide-btn slide-selected'><span>1</span></div>
-                        <div className='slide-btn'><span>2</span></div>
-                        <div className='slide-btn'><span>3</span></div>
-                        <div className='slide-btn'><span>4</span></div>
+                        <div
+                            onClick={() => { setCurrenSlide(0) }}
+                            className={!currentSlide ? 'slide-btn slide-selected' : 'slide-btn'}>
+                            <span>1</span>
+                        </div>
+                        <div
+                            onClick={() => { setCurrenSlide(1) }}
+                            className={currentSlide === 1 ? 'slide-btn slide-selected' : 'slide-btn'}>
+                            <span>2</span>
+                        </div>
+                        <div
+                            onClick={() => { setCurrenSlide(2) }}
+                            className={currentSlide === 2 ? 'slide-btn slide-selected' : 'slide-btn'}>
+                            <span>3</span>
+                        </div>
+                        <div
+                            onClick={() => { setCurrenSlide(3) }}
+                            className={currentSlide === 3 ? 'slide-btn slide-selected' : 'slide-btn'}>
+                            <span>4</span>
+                        </div>
                         {/* <div className='slide-btn'><span>5</span></div>
                         <div className='slide-btn'><span>6</span></div> */}
                     </div>
