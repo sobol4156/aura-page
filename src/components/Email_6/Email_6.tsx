b02dd133d5ff646240deed1c37506cbea3f23210
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-06 01:30:02 +0300

    decorated email

--- src/components/Email_6/Email_6.tsx
+++ src/components/Email_6/Email_6.tsx
@@ -1,21 +1,26 @@
+import { useState } from 'react'
 import './Email_6.less'
 
 const Email_6 = () => {
+    const [agree, setAgree] = useState(false)
     return (
         <section className='email'>
             <div className='email__content'>
                 <h2 className='email__title'>Будь в курсе новинок! Подпишись на рассылку!</h2>
-                <div className='email__inputs'>
-                    <input type="text" placeholder='Имя' />
-                    <input type="text" placeholder='E-mail' />
-                </div>
-                <button className='email__btn'>
-                    <span className='email__btn-text'>Подписаться</span>
-                </button>
-                <div className='email__contract'>
-                    <input type="checkbox" />
-                    <span>Согласен на обработку персональных данных</span>
-                </div>
+                <form action="" className='email__form'>
+                    <div className='email__inputs'>
+                        <input type="name" placeholder='Имя' />
+                        <input type="email" placeholder='E-mail' />
+                    </div>
+                        {/* взял из Auction_3 */}
+                        <button className='email__btn auction__bet-btn btn--fill'>
+                            <span>Подписаться</span>
+                        </button>
+                    <div className='email__contract'>
+                        <span onClick={() => {setAgree(!agree)}} className={`checkbox ${agree ? 'checkbox__active' : ''}`}></span>
+                        <span className='checkbox__text'>Согласен на обработку персональных данных</span>
+                    </div>
+                </form>
             </div>
         </section>
     )
