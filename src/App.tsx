a96cf30ec591243ac752ae6f8c7118ece7f5e224
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-06 01:52:09 +0300

    email adaptive

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ function App() {
         <Header />
       </header>
       <main>  
-            {/* <PlatformOfStickers_2/>
+            <PlatformOfStickers_2/>
             <Aiction_3/>
-            <OtherAuction_4/> */}
+            <OtherAuction_4/>
             <Slider_5/>
             <Email_6/>
       </main>
