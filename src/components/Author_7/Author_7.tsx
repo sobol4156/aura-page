2cb018cfb8ad8eda720931cd08a90b92b98ac010
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-06 05:56:11 +0300

    lint

--- src/components/Author_7/Author_7.tsx
+++ src/components/Author_7/Author_7.tsx
@@ -1,3 +1,4 @@
+
 import './Author_7.less'
 
 const Author_7 = () => {
@@ -6,8 +7,13 @@ const Author_7 = () => {
             <div className='author__content'>
                 <div className='author__text'>
                     <h2 className='author__title'>Стать автором</h2>
-                    <p className='author__paragraph-1'>Если ты художник или стремишься им стать — добро пожаловать к нам!</p>
-                    <p className='author__paragraph-2'>Отправь нам от трёх до пяти своих работ и напиши свой ник в телеграме. Если наши кураторы решат, что нам по пути, то мы обязательно свяжемся с тобой и обговорим условия сотрудничества! <strong>Удачи!</strong></p>
+                    <p className='author__paragraph-1'>Если ты художник или стремишься
+                        им стать — добро пожаловать к нам!</p>
+                    <p className='author__paragraph-2'>Отправь нам от трёх до пяти своих работ и напиши
+                        свой ник в телеграме. Если наши кураторы решат, что нам по пути, то мы обязательно
+                        свяжемся c тобой и обговорим условия сотрудничества!
+                        <strong>Удачи!</strong>
+                    </p>
                     <img src="https://live.verstaem.online/aura/img/become-decorate.png" alt="" />
                 </div>
                 <div className='author__form'>
