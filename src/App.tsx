9f0fc975210ace5ec31ed7c427a654aa896f51c6
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-06 20:16:46 +0300

    seller html+css

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import OtherAuction_4 from './components/OtherAuction_4/OtherAuction_4'
 import Slider_5 from './components/Slider_5/Slider_5'
 import Email_6 from './components/Email_6/Email_6'
 import Author_7 from './components/Author_7/Author_7'
+import TopSeller_8 from './components/TopSeller_8/TopSeller_8'
 
 function App() {
 
@@ -17,12 +18,13 @@ function App() {
         <Header />
       </header>
       <main>  
-            <PlatformOfStickers_2/>
+            {/* <PlatformOfStickers_2/>
             <Aiction_3/>
             <OtherAuction_4/>
             <Slider_5/>
-            <Email_6/>
+            <Email_6/> */}
             <Author_7/>
+            <TopSeller_8/>
       </main>
     </>
   )
---
+++ src/components/TopSeller_8/TopSeller_8.tsx
@@ -0,0 +1,93 @@
+import './TopSeller_8.less'
+
+const TopSeller_8 = () => {
+    return (
+        <section className='seller'>
+            <div className='seller__content'>
+                <h2 className='seller__title'>Топ продаж</h2>
+                <ul className='seller__list'>
+                    <li className='seller__item'>
+                        <div className='seller__item-img'>
+                            <img src="https://live.verstaem.online/aura/img/top-1.png" alt="" />
+                        </div>
+                        <div className='seller__item-info'>
+                            <div className='seller__item-header'>
+                                <img src="	https://live.verstaem.online/aura/img/avatar-1.jpg" alt="profile" />
+                                <span>ttgs</span>
+                            </div>
+                            <h3 className='seller__item-name'>Holofobia1</h3>
+                            <div className='seller__item-price'>4.70 ETH <span>($16,873.47)</span></div>
+                        </div>
+                    </li>
+                    <li className='seller__item'>
+                        <div className='seller__item-img'>
+                            <img src="https://live.verstaem.online/aura/img/top-2.png" alt="" />
+                        </div>
+                        <div className='seller__item-info'>
+                            <div className='seller__item-header'>
+                                <img src="https://live.verstaem.online/aura/img/avatar-2.jpg" alt="profile" />
+                                <span>ttgs</span>
+                            </div>
+                            <h3 className='seller__item-name'>Holofobia1</h3>
+                            <div className='seller__item-price'>4.70 ETH <span>($16,873.47)</span></div>
+                        </div>
+                    </li>
+                    <li className='seller__item'>
+                        <div className='seller__item-img'>
+                            <img src="https://live.verstaem.online/aura/img/top-3.png" alt="" />
+                        </div>
+                        <div className='seller__item-info'>
+                            <div className='seller__item-header'>
+                                <img src="https://live.verstaem.online/aura/img/avatar-3.jpg" alt="profile" />
+                                <span>ttgs</span>
+                            </div>
+                            <h3 className='seller__item-name'>Holofobia1</h3>
+                            <div className='seller__item-price'>4.70 ETH <span>($16,873.47)</span></div>
+                        </div>
+                    </li>
+                    <li className='seller__item'>
+                        <div className='seller__item-img'>
+                            <img src="https://live.verstaem.online/aura/img/top-4.png" alt="" />
+                        </div>
+                        <div className='seller__item-info'>
+                            <div className='seller__item-header'>
+                                <img src="https://live.verstaem.online/aura/img/avatar-4.jpg" alt="profile" />
+                                <span>ttgs</span>
+                            </div>
+                            <h3 className='seller__item-name'>Holofobia1</h3>
+                            <div className='seller__item-price'>4.70 ETH <span>($16,873.47)</span></div>
+                        </div>
+                    </li>
+                    <li className='seller__item'>
+                        <div className='seller__item-img'>
+                            <img src="https://live.verstaem.online/aura/img/top-5.png" alt="" />
+                        </div>
+                        <div className='seller__item-info'>
+                            <div className='seller__item-header'>
+                                <img src="https://live.verstaem.online/aura/img/avatar-5.jpg" alt="profile" />
+                                <span>ttgs</span>
+                            </div>
+                            <h3 className='seller__item-name'>Holofobia1</h3>
+                            <div className='seller__item-price'>4.70 ETH <span>($16,873.47)</span></div>
+                        </div>
+                    </li>
+                    <li className='seller__item'>
+                        <div className='seller__item-img'>
+                            <img src="https://live.verstaem.online/aura/img/top-6.png" alt="" />
+                        </div>
+                        <div className='seller__item-info'>
+                            <div className='seller__item-header'>
+                                <img src="https://live.verstaem.online/aura/img/avatar-6.jpg" alt="profile" />
+                                <span>ttgs</span>
+                            </div>
+                            <h3 className='seller__item-name'>Holofobia1</h3>
+                            <div className='seller__item-price'>4.70 ETH <span>($16,873.47)</span></div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default TopSeller_8
