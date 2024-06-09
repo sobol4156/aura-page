f222eb5d69cb197dfa62351e365ffc1fd01d3f92
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-09 20:05:48 +0300

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Email_6 from './components/Email_6/Email_6'
 import Author_7 from './components/Author_7/Author_7'
 import TopSeller_8 from './components/TopSeller_8/TopSeller_8'
 import Hit_9 from './components/Hit_9/Hit_9'
+import Footer_10 from './components/Footer_10/Footer_10'
 
 function App() {
 
@@ -28,6 +29,9 @@ function App() {
             <TopSeller_8/>
             <Hit_9/>
       </main>
+      <footer>
+        <Footer_10/>
+      </footer>
     </>
   )
 }
